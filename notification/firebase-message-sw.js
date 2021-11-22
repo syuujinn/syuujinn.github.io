b38321a9ec530c7e549804bda13b5c4a63143e9d
syuujinn <syuujinn@gmail.com>
2021-11-22 09:06:41 +0800

    Add files via upload
    new pages

---
+++ notification/firebase-message-sw.js
@@ -0,0 +1,43 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+
+const applicationServerPublicKey = "BKHhlP2eWKSL_EPHzwftHJ0hwaA3yZb2y2Fa8Y4AKedk4UWQY8xKNQuGDuFeCZ9AHldL9yz6979uYqjv6V3tuIg";
+const config = {
+    apiKey: "AIzaSyA-V8Tj7Qzm6pAMc-mfg9Dj_xmFG1OThiA",
+    authDomain: "dpush-2fdbf.firebaseapp.com",
+    databaseURL: "https://dpush-2fdbf.firebaseio.com",
+    projectId: "dpush-2fdbf",
+    storageBucket: "dpush-2fdbf.appspot.com",
+    messagingSenderId: "204581970791",
+    appId: "1:204581970791:web:9113bdf353088a2bb645c2",
+    measurementId: "G-EW724P3LZX"
+};
+firebase.initializeApp(config);
+
+//網頁 背景處理部分的(以chrome 為例 就是win 右下角會持續運作)
+//註冊 準備推送訊息給使用者
+
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey(applicationServerPublicKey);
+console.log(self.registration.showNotification, self, this);
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log('Handling background message ', payload);
+
+  return self.registration.showNotification(payload.data.title, {
+    body: payload.data.body,
+    icon: payload.data.icon,
+    tag: payload.data.tag,
+    data: payload.data,
+    url : payload.data.url
+  });
+});
+
+self.addEventListener('notificationclick', event => {
+    const notification = event.notification;
+    const action = event.action;
+    const link = notification.data.url;
+    clients.openWindow(link);
+    notification.close();
+    console.log('notificationclick action is', action);
+  })
